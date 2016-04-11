21c90858fe1bfa1976b41a1abd6dfa951f40808e
Oleh Rudniy <oleh_rudniy@epam.com>
2016-04-11 13:01:25 +0300

    Calculator

--- less2/js/task6.js
+++ less2/js/task6.js
@@ -1,3 +1,38 @@
+function zero(callback) {
+    if (callback) {
+        return callback(0);
+    } else {
+        return 0;
+    }
+}
+function one(callback) {
+    if (callback) {
+        return callback(1);
+    } else {
+        return 1;
+    }
+}
+function two(callback) {
+    if (callback) {
+        return callback(2);
+    } else {
+        return 2;
+    }
+}
+function three(callback) {
+    if (callback) {
+        return callback(3);
+    } else {
+        return 3;
+    }
+}
+function four(callback) {
+    if (callback) {
+        return callback(4);
+    } else {
+        return 4;
+    }
+}
 function five(callback) {
     if (callback) {
         return callback(5);
@@ -5,12 +40,46 @@ function five(callback) {
         return 5;
     }
 }
-
-function three(callback) {
+function six(callback) {
     if (callback) {
-        return callback(3);
+        return callback(6);
     } else {
-        return 3;
+        return 6;
+    }
+}
+function seven(callback) {
+    if (callback) {
+        return callback(7);
+    } else {
+        return 7;
+    }
+}
+
+function eight(callback) {
+    if (callback) {
+        return callback(8);
+    } else {
+        return 8;
+    }
+}
+
+function nine(callback) {
+    if (callback) {
+        return callback(9);
+    } else {
+        return 9;
+    }
+}
+
+function plus(firstOperand) {
+    return function (secondOperand) {
+        return firstOperand + secondOperand;
+    }
+}
+
+function minus(firstOperand) {
+    return function (secondOperand) {
+        return secondOperand - firstOperand;
     }
 }
 
@@ -20,4 +89,17 @@ function multiply(firstOperand) {
     }
 }
 
-three(multiply(five()));
+function divide(firstOperand) {
+    return function (secondOperand) {
+        return secondOperand / firstOperand;
+    }
+}
+
+three(multiply(five())); // 15
+three(plus(five())); // 8
+three(minus(five())); // 2
+eight(divide(two())); // 4
+seven(multiply(five())); // 35
+four(plus(nine())); // 13
+eight(minus(three())); // 5
+six(divide(two())); // 3
