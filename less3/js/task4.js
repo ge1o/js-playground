5d69d97f8abcf1abb03fa95247740d2e3673a59f
Oleh Rudniy <oleh_rudniy@epam.com>
2016-04-11 13:50:21 +0300

    Resize

--- less3/js/task4.js
+++ less3/js/task4.js
@@ -1,3 +1,39 @@
 //4) Функция которая во время изменения размера окна браузера будет показывать размер
 //окна. Размер экрана выводить только если пользователь не делает изменений размера экрана
 //более 2 секунд.
+
+(function() {
+    var newSizeWidth;
+    var newSizeHeight;
+    var delay;
+    var sizeContainer = window.document.querySelector('.size');
+
+    var actualSize = {
+        width: 0,
+        height: 0
+    };
+
+    setSize();
+
+    window.onresize = function() {
+        if (delay != null) {
+            clearTimeout(delay);
+        }
+
+        delay = setTimeout(function(){
+            newSizeWidth = window.document.body.offsetWidth;
+            newSizeHeight = window.document.body.offsetHeight;
+            if (actualSize.width != newSizeWidth || actualSize.height != newSizeWidth) {
+                setSize();
+            }
+
+        }, 2000);
+    };
+
+    function setSize() {
+        actualSize.width = window.document.body.offsetWidth;
+        actualSize.height = window.document.body.offsetHeight;
+        sizeContainer.innerText = actualSize.width + ' × ' + actualSize.height;
+    }
+
+}).call();
